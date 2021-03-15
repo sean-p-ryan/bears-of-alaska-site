d528d2b46871147ab2c16e45d948b08c56504be0
Sean Ryan <seanryan@Seans-MacBook-Pro.local>
2021-03-15 00:10:25 -0400

    Added meta tags, fixed Bandcamp URL

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-const navToggle = document.querySelector('#nav-toggle');
+const navToggle = document.querySelector('.nav-mobile');
 const navItems = document.querySelector('.nav-items');
+const navLinks = document.querySelectorAll('.nav-item');
 
 function toggleActive() {
     navToggle.classList.toggle('active')
     navItems.classList.toggle('visible')
 }
 
-navToggle.onclick = toggleActive
+function addOnclicks(){
+    console.log()
+    navToggle.onclick = toggleActive
+    Array.from(navLinks).forEach(function(link) {
+        link.onclick = toggleActive
+    })
+}
+
+addOnclicks()
 
