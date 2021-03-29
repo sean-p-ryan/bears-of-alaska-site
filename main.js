4d575c7b7bfc7d39cb32ea1fe420aa568af289d9
Sean Ryan <seanryan@Seans-MacBook-Pro.local>
2021-03-28 22:11:25 -0400

    Added about page, contact show/hide works

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const mobileNavIcon = document.querySelector('.nav-mobile')
 const navItems = document.querySelector('.nav-items');
 const navLinks = document.querySelectorAll('.nav-item');
+const contactLink = document.querySelector('.contact-link');
+const overlay = document.querySelector('.overlay');
+const body = document.getElementsByTagName("BODY")[0]; 
+let emailVisible = false;
 
 document.addEventListener('click', function (e) {
     if (window.innerWidth <= 769) {
@@ -14,4 +18,30 @@ document.addEventListener('click', function (e) {
         }
     }
 })
+
+document.addEventListener('click', function(e){
+    if(e.target.classList.contains('contact-link')){
+        if (window.innerWidth <= 1000){
+            contactLink.classList.add('smaller-text')
+        }
+        if(!emailVisible){
+            contactLink.innerText = "bearsofalaskamusic.com";  
+            overlay.style.visibility = "visible";
+            body.style.overflowY = "hidden";
+        } else {
+            contactLink.innerText = "Contact";  
+            overlay.style.visibility = "hidden";
+            body.style.overflowY = "auto";
+            contactLink.classList.remove('smaller-text')
+        }
+        emailVisible = !emailVisible;        
+    }
+})
+
+// document.addEventListener('mouseout', function(e){
+//     if(e.target.classList.contains('contact-link')){
+//         contactLink.innerText = "Contact";    
+//         contactTextContainer.style.witdth = "100vw";
+//     }
+// })
 
